0869a343be2e76db4d1b81e11a3e7ac7a8fc6fe3
Hanna Johansson <hannajohansson16@hotmail.com>
2026-03-26 14:19:40 +0100

    Add MatchingPage route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import RegisterPage from './pages/RegisterPage'
 import RulesPage from './pages/RulesPage'
+import MatchingPage from './pages/MatchingPage'
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<RegisterPage />} />
         <Route path="/rules" element={<RulesPage />} />
+        <Route path="/matching" element={<MatchingPage />} />
+
       </Routes>
     </BrowserRouter>
   )
