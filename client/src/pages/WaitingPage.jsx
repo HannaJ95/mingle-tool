b905a71c83c1676b6787972dc502b56b42f6df02
AllanTran90 <allan_sweden90@hotmail.com>
2026-03-30 13:08:54 +0200

    feat: add WaitingPage with placeholder

--- client/src/pages/WaitingPage.jsx
+++ client/src/pages/WaitingPage.jsx
@@ -5,22 +5,16 @@ import PageLayout from "../components/layout/PageLayout";
 export default function WaitingPage(){
     return(
         <PageLayout>
-            <div className="max-w-sm w-full mx-auto min-h-screen flex flex-col items-center justify-center text-center py-24 gap-14">
+            <div className="max-w-sm w-full mx-auto min-h-screen flex flex-col items-center justify-between text-center py-24 gap-14">
         
                 <Text as="h1">
                     We are finding your Group!
                 </Text>
 
-                {/* You can change the information to the actual mp4 */}
-                <video
-                    src="/assets/moth.mp4"
-                    autoPlay
-                    loop
-                    muted
-                    playsInline
-                    className="w-48 opacity-90"
-                />
-
+               {/* Will be replaced with actual mp4 animation */}
+                <div className="w-72 h-72 bg-gray-200 rounded-md flex items-center justify-center">
+                  <span className="text-xs text-gray-500">Loading...</span>
+                </div>
 
 
 
