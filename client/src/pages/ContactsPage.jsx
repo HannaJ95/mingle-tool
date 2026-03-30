8f7ab7d393dcb40bcc68089ee4ec017f7524210c
AllanTran90 <allan_sweden90@hotmail.com>
2026-03-30 12:27:40 +0200

    feat: add ContactsPage layout and styling

--- client/src/pages/ContactsPage.jsx
+++ client/src/pages/ContactsPage.jsx
@@ -14,21 +14,22 @@ export default function ContactsPage() {
 
   return (
     <PageLayout>
-      <div className="max-w-xl mx-auto px-6 py-14 flex flex-col gap-14">
+      <div className="max-w-xl mx-auto py-14 flex flex-col gap-14">
 
-        
+      <div className="px-6">  
         <Text as="h1">
           CONNECT
         </Text>
+      </div>
 
         {/* List */}
         <div className="flex flex-col gap-10">
           {users.map((user) => (
-            <div key={user.id} className="flex items-center justify-between">
+            <div key={user.id} className="flex items-center justify-between pl-6 pr-4">
 
               
-              <div className="flex items-center gap-3">
-                <span className="text-2xl">⭐</span>
+              <div className="flex items-center justify-between -mx-6 px-6">
+                <span className="flex items-center gap-5">⭐</span>
 
                 <div>
                   <p className="font-medium">{user.name}</p>
@@ -37,19 +38,21 @@ export default function ContactsPage() {
               </div>
 
               
-              <Button variant="secondary mx-4 my-4">
+              <Button variant="connect"
+                      className="ml-auto !bg-[#gray-300] !text-black !border !border-black !rounded-md !px-3 !py-1 !text-xs">
                 CONNECT
               </Button>
             </div>
           ))}
         </div>
 
-        
-        <Button>
+      <div className="px-6">
+        <Button variant="secondary" onClick={() => alert('Start new game')}>
           START NEW GAME
         </Button>
-
       </div>
+      </div>
+      
     </PageLayout>
   );
 }
