fdc333b7091f48daa472273c8ce1af37b3b9d8d4
AllanTran90 <allan_sweden90@hotmail.com>
2026-03-31 20:25:40 +0200

    feat: setup express + socket.io server with basic matchmaking

--- server/index.js
+++ server/index.js
@@ -1,12 +1,66 @@
-import express from 'express';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
+// make an express app
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const CLIENT_URL = "http://localhost:5173"
+
+//  Middleware
+app.use(cors({
+  origin: CLIENT_URL
+}));
+
+app.use(express.json());
+
+//  HTTP server 
+const server = http.createServer(app);
+
+// Socket.IO
+const io = new Server(server, {
+  cors: {
+    origin: CLIENT_URL
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/", (req, res) => {
+  res.send("Server is running ");
+});
+
+//  Matchmaking state
+let waitingPlayer = null;
+
+// socket logic
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  // matchmaking
+  if (waitingPlayer) {
+    console.log("Match found!");
+
+    socket.emit("match", { opponent: waitingPlayer.id });
+    waitingPlayer.emit("match", { opponent: socket.id });
+
+    waitingPlayer = null;
+  } else {
+    console.log("Waiting for another player...");
+    waitingPlayer = socket;
+  }
+
+   // disconnect
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+
+    if (waitingPlayer === socket) {
+      waitingPlayer = null;
+    }
+  });
+});
+
+//  Start server
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
