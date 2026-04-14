8c21d193f02d6cbf3e64e237470ea9fe7dadcb13
AllanTran90 <allan_sweden90@hotmail.com>
2026-04-14 14:12:48 +0200

    feat: integrate socket matchmaking with Zustand in WaitingPage

--- client/src/features/waiting/WaitingPage.jsx
+++ client/src/features/waiting/WaitingPage.jsx
@@ -2,20 +2,30 @@ import Text from "../../components/ui/Text.jsx";
 import { useEffect } from "react";
 import socket from "../../socket";
 import { useNavigate } from "react-router";
+import useAppStore from "../../store/useAppStore";
 
 export default function WaitingPage() {
   const navigate = useNavigate();
 
-useEffect(() => {
-  // const user = { name: "Allan" };
-  const user = JSON.parse(sessionStorage.getItem("user"));
+  const user = useAppStore((state) => state.user) || 
+  JSON.parse(sessionStorage.getItem("user") || "null");
 
-  console.log("USER SENT:", user);
+  const setGroup = useAppStore((state) => state.setGroup);
+  
+
+useEffect(() => {
+  if (!user) return; 
+
+  console.log("Joining queue as:", user);
+
   socket.emit("joinQueue", user);
 
-  const handleGroupReady = (data) => {
-    console.log("Matched!", data);
-    navigate("/matching", { state: data });
+  const handleGroupReady = (group) => {
+    console.log("GROUP FROM SERVER:", group);
+
+    setGroup(group);
+
+    navigate("/matching");
   };
 
   socket.on("groupReady", handleGroupReady);
@@ -23,9 +33,9 @@ useEffect(() => {
   return () => {
     socket.off("groupReady", handleGroupReady);
   };
-}, []);
-//   return <div>Waiting for players...</div>;
-// }
+}, [user]);
+
+  //   return <div>Waiting for players...</div>;
   return (
       <div className="min-w-80 max-w-screen flex flex-col items-center">
       <main className="min-h-screen w-full max-w-md flex flex-col justify-center gap-10 xs:gap-14">
